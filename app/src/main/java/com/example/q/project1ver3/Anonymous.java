cd017f22a7e660b0b0f101f76ad60d5b3cd9d18b
NaHut <whow1111@naver.com>
2018-07-04 20:21:42 +0900

    Implement google map

--- app/src/main/java/com/example/q/project1ver3/Anonymous.java
+++ app/src/main/java/com/example/q/project1ver3/Anonymous.java
@@ -16,6 +16,8 @@ public class Anonymous extends Fragment {
         super.onCreate(savedInstanceState);
         View rootView = inflater.inflate(R.layout.anonymous, container, false);
         Button gotobtn = (Button) rootView.findViewById(R.id.worldcupbtn);
+        Button mapBtn =(Button) rootView.findViewById(R.id.map_btn);
+
         gotobtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +26,16 @@ public class Anonymous extends Fragment {
                 context.startActivity(intent);
             }
         });
+
+        mapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getContext();
+                Intent intent = new Intent(context, MapsActivityCurrentPlace.class);
+                context.startActivity(intent);
+            }
+        });
+
         return rootView;
     }
 }
